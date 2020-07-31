0a707afc518e0fee9cb32ee7d6b7c8df58fc7513
Sky Moore <mskymoore@gmail.com>
2020-07-31 14:04:43 -0500

    fix routes so current route name is available add customizable plus button behavior

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -6,24 +6,46 @@ import 'package:tem_app/views/initial.dart';
 import 'package:tem_app/views/worklog.dart';
 
 class RouteGenerator {
+  static const String root = '/';
+  static const String login = '/login';
+  static const String worklog = '/worklog';
+  static const String create_worklog = '/create_worklog';
+  static const String client = '/client';
+
   static Route<dynamic> generateRoute(RouteSettings settings) {
     //final args = settings.arguments;
 
     switch (settings.name) {
-      case '/':
-        return MaterialPageRoute(builder: (_) => IsLoginRequired());
-      case '/login':
+      case root:
         return MaterialPageRoute(
-            builder: (_) => LoginPage(title: "TEM LOG IN"));
-      case '/worklog':
-        return MaterialPageRoute(builder: (_) => MainPage("TEM APP BETA"));
-      case '/create_worklog':
-        return MaterialPageRoute(builder: (_) => WorklogPage("Create Worklog"));
-      case '/client':
-        return MaterialPageRoute(builder: (_) => ClientPage("TEM APP BETA"));
+          builder: (_) => IsLoginRequired(),
+          settings: RouteSettings(name: root),
+        );
+      case login:
+        return MaterialPageRoute(
+          builder: (_) => LoginPage(title: "TEM LOG IN"),
+          settings: RouteSettings(name: login),
+        );
+      case worklog:
+        return MaterialPageRoute(
+          builder: (_) => MainPage("TEM APP BETA"),
+          settings: RouteSettings(name: worklog),
+        );
+      case create_worklog:
+        return MaterialPageRoute(
+          builder: (_) => WorklogPage("Create Worklog"),
+          settings: RouteSettings(name: create_worklog),
+        );
+      case client:
+        return MaterialPageRoute(
+          builder: (_) => ClientPage("TEM APP BETA"),
+          settings: RouteSettings(name: client),
+        );
       default:
         return MaterialPageRoute(
-            builder: (_) => LoginPage(title: "TEM LOG IN"));
+          builder: (_) => LoginPage(title: "TEM LOG IN"),
+          settings: RouteSettings(name: login),
+        );
     }
   }
 }
--- lib/widgets/scaffold.dart
+++ lib/widgets/scaffold.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:tem_app/widgets/drawer.dart';
 
+void plusButtonBehavior(context) {
+  final currentRoute = ModalRoute.of(context).settings.name;
+  if (currentRoute == '/worklog' || currentRoute == '/') {
+    Navigator.of(context).pushNamed('/create_worklog');
+  } else if (currentRoute == '/create_worklog') {
+    print(currentRoute);
+    return;
+  } else {
+    print(currentRoute);
+    return;
+  }
+}
+
 class TemScaffold extends StatelessWidget {
   final String title;
   final Widget body;
@@ -18,7 +31,7 @@ class TemScaffold extends StatelessWidget {
             body: this.body,
             floatingActionButton: FloatingActionButton(
               onPressed: () {
-                Navigator.of(context).pushNamed('/create_worklog');
+                plusButtonBehavior(context);
               },
               child: Icon(Icons.add),
               backgroundColor: Colors.green,
